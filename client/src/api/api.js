7c0cdfd5998a243bfbb5481620e563b602ca2f16
Shelton Francisco <sheltonfjs@gmail.com>
2023-06-09 09:34:27 +0200

    update to prod

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-// const prodUrl = "https://voting-server.onrender.com/api";
-const devUrl = "http://localhost:3000/api";
+const serverUrl = "https://voting-server.onrender.com/api";
+// const serverUrl = "http://localhost:3000/api";
 const api = axios.create({
-  baseURL: devUrl,
+  baseURL: serverUrl,
 });
 
 export default api;
