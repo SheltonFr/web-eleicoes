20763582edefa37d35334c32c6e40c029981d17c
Shelton Francisco <sheltonfjs@gmail.com>
2023-06-09 13:19:54 +0200

    merge

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const serverUrl = "https://voting-server.onrender.com/api";
-const serverUrl = "http://localhost:3000/api";
+const serverUrl = "https://voting-server.onrender.com/api";
+// const serverUrl = "http://localhost:3000/api";
 const api = axios.create({
   baseURL: serverUrl,
 });
